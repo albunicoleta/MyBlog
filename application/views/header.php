5eaeee5f751d58496fc0c507a953d871b91909ee
albunicoleta <albunicoleta@gmail.com>
2012-10-02 20:44:48 +0300

    created header view

---
+++ application/views/header.php
@@ -0,0 +1,5 @@
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" type="text/css"  media="all" />
+    </head>
