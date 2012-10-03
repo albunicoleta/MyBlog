ba3c36434ecbe1cb469c96dbae5d9cdf247b5daa
albunicoleta <albunicoleta@gmail.com>
2012-10-03 13:26:16 +0300

    created div

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,12 +1,16 @@
     <body>
-        <form method="post" action="postCreate"> 
-            <label for="author">Author</label>
-            <input type="text" name="author" />
-            <label for="title">Title</label>
-            <input type="text" name="title" />
-            <label for="content">Content</label>
-            <textarea name="content"></textarea>
-            <input type="submit" value="Post" />
-        </form>
+        <div id="wrapper">
+            <h1>Welcome to my blog!</h1>
+            <p>Please write your posts bellow!</p>
+            <form method="post" action="postCreate"> 
+                <div><label for="author">Author</label></div>
+                <input type="text" name="author" />
+                <div><label for="title">Title</label></div>
+                <input type="text" name="title" />
+                <div><label for="content">Content</label></div>
+                <textarea name="content"></textarea>
+                <div id="submit"><input type="submit" value="Post" /></div>
+            </form>
+        </div>
     </body>  
 </html>
