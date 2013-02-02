806590ba9d0604866550144c4aa3e04714fcba1e
albunicoleta <albunicoleta@gmail.com>
2013-02-02 14:27:13 +0200

    This is just a test commit from NB

--- application/views/header.php
+++ application/views/header.php
@@ -3,3 +3,4 @@
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css" type="text/css"  media="all" />
     </head>
+<!-- this is a test -->
